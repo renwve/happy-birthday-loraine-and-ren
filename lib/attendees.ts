9e33c1ae741e0dd16b0f36fa3ecd53fb175a7165
Ren Macatuno <renmacatuno@gmail.com>
2026-09-23 15:12:32 -0600

    Change guest names

--- lib/attendees.ts
+++ lib/attendees.ts
@@ -1,6 +1,6 @@
 export const ATTENDEES = [
-  'Guest 1', 'Guest 2', 'Guest 3', 'Guest 4',
-  'Guest 5', 'Guest 6', 'Guest 7',
+  'Loraine', 'Ren', 'Alexa', 'Eli',
+  'Marie', 'Shayla', 'Sophia',
 ] as const;
 
 export type AttendeeName = (typeof ATTENDEES)[number];
